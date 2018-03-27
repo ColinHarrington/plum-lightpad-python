1c30038f44e5a7ab8c46175c12b5f8b688d779c2
Heath Paddock <hp@heathpaddock.com>
2018-03-27 10:32:28 -0500

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
-import os
-import re
 from setuptools import setup
-
-REQUIRES = []
 
 PACKAGE_NAME = 'plumlightpad'
 
 setup(
     name = PACKAGE_NAME,
-    version = "0.0.7",
+    version = "0.0.8",
     author = "Heath Paddock",
     author_email = "hp@heathpaddock.com",
     description = ("A python package that interacts with the Plum Lightpad"),
     license = "MIT",
     keywords = ["plum", "lightpad"],
     url = "https://github.com/heathbar/plum-lightpad",
-    packages=['plumlightpad'],
-    include_package_data=True,
-    install_requires=REQUIRES,
-    classifiers=[
+    packages = ['plumlightpad'],
+    include_package_data = True,
+    classifiers = [
         'Intended Audience :: Developers',
         "Development Status :: 3 - Alpha",
         "Topic :: Home Automation",
