7074d2914bcd7c47a044815eeef3a1613562bb2c
Colin Harrington <colin.harrington@gmail.com>
2018-09-27 16:46:22 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PACKAGE_NAME = 'plumlightpad'
 
 setup(
     name = PACKAGE_NAME,
-    version = "0.0.9",
+    version = "0.0.10",
     author = "Heath Paddock",
     author_email = "hp@heathpaddock.com",
     description = ("A python package that interacts with the Plum Lightpad"),
